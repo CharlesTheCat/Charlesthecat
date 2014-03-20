063278567d9428e40891de1c51e61c5fb7ae6a90
Neil Berard <neilberard@gmail.com>
2014-03-19 22:01:28 -0700

    add rigidbody

---
+++ Project/Assets/Player/Scripts/PlayerRigidCtrl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+public class PlayerRigidCtrl : MonoBehaviour {
+
+	public float speed = 10.0f;
+	public float gravity = 10.0f;
+
+	void Awake (){
+	
+	}
+
+	public Rigidbody RCtrl;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	//RCtrl = GetComponent(Rigidbody);
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey(KeyCode.W)){
+			rigidbody.AddForce(0,0,speed); 
+		}
+	}
+}
