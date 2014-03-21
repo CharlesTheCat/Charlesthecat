dab99774b4607b7d9033cfe25f50230f71ce5eb9
Neil Berard <neilberard@gmail.com>
2014-03-20 22:12:35 -0700

    ,added gun

--- Project/Assets/Player/Scripts/PlayerRigidCtrl.cs
+++ Project/Assets/Player/Scripts/PlayerRigidCtrl.cs
@@ -4,19 +4,21 @@ using System.Collections;
 [RequireComponent (typeof (Rigidbody))]
 
 public class PlayerRigidCtrl : MonoBehaviour {
-
+	public Rigidbody rocketPrefab;
+	public Transform barrelEnd;
 	public float speed = 10.0f;
 	public float gravity = 10.0f;
 	public float Horizontal;
 	private float Vertical;
 	public float TurnSpeed =10.0f;
-	public Vector3 TransformDirection;
+
+	//public Vector3 TransformDirection;
 
 	void Awake (){
 	
 	}
 
-	public Rigidbody RCtrl;
+
 	// Use this for initialization
 	void Start () {
 
@@ -25,16 +27,27 @@ public class PlayerRigidCtrl : MonoBehaviour {
 	//RCtrl = GetComponent(Rigidbody);
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update(){
+		if(Input.GetButtonDown("Fire1"))
+		{
+			Rigidbody rocketInstance;
+			rocketInstance = Instantiate(rocketPrefab, barrelEnd.position,barrelEnd.rotation) as Rigidbody;
+			rocketInstance.AddForce(barrelEnd.forward * -2000);
+			rigidbody.AddRelativeForce(0,0,200);
+			Debug.Log("bam");
+		}
 
+	}
+
+	void FixedUpdate () {
+		//Get Axis
 		Horizontal = Input.GetAxis("Horizontal");
 		Vertical = Input.GetAxis("Vertical");
 
 
 
 
-
-
+	
 		rigidbody.AddRelativeForce(0,0,Vertical * speed * -1); 
 
 
